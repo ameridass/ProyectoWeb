c71b421eb3758e582af137fa5acedbaf1d522128
Josue Alejandro Noriega Letran <alexelectricz@gmail.com>
2020-10-31 16:39:45 -0600

    jn31102020
    PUTO EL QUE LO LEA

---
+++ login_registrar.php
@@ -0,0 +1,24 @@
+<?php
+
+include("conexion.php");
+
+$nombre = $_POST["usuario"];
+$pass   = $_POST["pass"];
+
+//Login
+if(isset($_POST["btningresar"]))
+{
+	$query = mysqli_query($conn,"select ftn_login('$nombre','$pass')");
+	$nr = ($query);
+	
+	if($nr==1)
+	{
+		echo "<script> alert('Bienvenido $nombre'); window.location='index.html' </script>";
+	}else
+	{
+		echo "<script> alert('Usuario no existe'); window.location='index.html' </script>";
+	}
+}
+
+
+?>
