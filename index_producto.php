48d2fbf180f7b46f256099e1c2e6fbff22e8919d
Josue Alejandro Noriega Letran <alexelectricz@gmail.com>
2020-10-31 20:08:04 -0600

    jn_31102020
    cambios al php para creacion del producto

--- index_producto.php
+++ index_producto.php
@@ -7,22 +7,15 @@ include("conexion.php");
 $ProdNom = $_POST["ProdNom"];
 $categoría   = $_POST["categoría"];
 $cantidad = $_POST["cantidad"];
+$imagen = $_POST["imagen"];
 $descripcion   = $_POST["descripcion"];
 
 
-//Login
+
 if(isset($_POST["btnguardar"]))
 {
 	$query = mysqli_query($conn,"select ftn_login('$nombre','$pass')");
 	$nr = ($query);
-	
-	if($nr==1)
-	{
-		echo "<script> alert('Bienvenido $nombre'); window.location='index.html' </script>";
-	}else
-	{
-		echo "<script> alert('Usuario no existe'); window.location='index.html' </script>";
-	}
+	echo "<script> alert('$nr'); window.location='index_producto.html' </script>";
 }
-
 ?>
