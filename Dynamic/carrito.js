d50e39fc360c46ce1f5cc5f793c12fbf1afbaa77
unknown <geizer7@galileo.edu>
2020-11-07 17:38:24 -0600

    Nuevo Carrito

---
+++ Dynamic/carrito.js
@@ -0,0 +1,45 @@
+
+class carro{
+
+    selccionarProducto(e){
+        e.preventDefault();
+
+                const producto = e.target.parentElement.parentElement;
+                console.log(producto);
+                this.mostrarDatos(producto);
+    
+    
+    
+    }
+
+    mostrarDatos(producto){
+
+        const infoProducto ={
+            imagen : producto.querySelector('img').src,
+            precio : producto.querySelector('p').textContent
+        }
+
+        this.insertarCarrito(infoProducto);
+        
+        localStorage.setItem("imagenProducto", infoProducto.titulo);
+            console.log(infoProducto.titulo);
+        
+        
+        }
+
+        insertarCarrito(articulo){
+
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                <td> <img src = "${articulo.imagen}"width =100 >
+                </td>
+                
+                `;
+                
+
+        }
+
+
+
+
+}
