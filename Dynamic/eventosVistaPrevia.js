b6bfa7c1af9d744b9bebf36be2efaaad82bae3fb
Alvaro Merida <ameridas1998@gmail.com>
2020-11-14 01:11:53 -0600

    Re diseño index para realizar pedidos al carrito

--- Dynamic/eventosVistaPrevia.js
+++
@@ -1,9 +0,0 @@
-const d = new vista();
-console.log("SI RESPONDE METODO");
-const p = document.getElementById('art');
-
-cargarEventos();
-
-function cargarEventos(){
-    p.addEventListener('click', (e)=>{d.selccionarProducto(e)});
-}
