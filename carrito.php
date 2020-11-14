8c7dbe53cbfb061565b201403d34268faf0008f0
Josue Alejandro Noriega Letran <alexelectricz@gmail.com>
2020-11-13 22:35:18 -0600

    JN_13112020_2
    prueba

--- carrito.php
+++ carrito.php
@@ -1,28 +1,5 @@
 
 
 <?php
-include("conexion.php");
-
-$cantidad = $_POST["cantidad"];
-$Prodcod = $_POST["procod"];
-$cliente = $_post["user"];
-if(isset($_POST["añadir"]))
-{
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "INSERT INTO tordenes(`tpronom`,`tprocat`,`tprodsc`,
-                         `tprofec`,`tprosts`,`tproimg`)
-                    VALUES
-                    ('$Prodcod','$categoria','$descripcion',$fecha,'A','$imagen');";
-
-if ($conn->query($sql) === TRUE) {
-  echo "Dato Ingresado correctamente";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-}
+include_once 'index.php'
 ?>
